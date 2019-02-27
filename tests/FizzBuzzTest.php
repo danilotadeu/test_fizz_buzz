54a358eb16e2f360155293dfb738d8fa53fdb216
danilo <danilo@e-htl.com.br>
2019-02-27 17:36:05 -0300

    test danilo

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -10,7 +10,7 @@ class FizzBuzzTest extends TestCase
 {
     const STRING_RESULT = '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17';
 
-    public function testPushAndPop()
+    public function testFizzBuzz()
     {
         $fizzBuzz = new \FizzBuzz\FizzBuzz;
         $result = $fizzBuzz->getFizzBuzz(17);
